ae7e3241e5eae1ca916a7080bdeab5a7c9f7c83a
M SRAVANA LAKSHMI <sravanamajji6@gmail.com>
2025-12-22 09:43:44 +0530

    Time: 231 ms (18.18%), Space: 12.4 MB (100%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDeletionSize(self, strs):
+        n = len(strs)
+        m = len(strs[0])
+
+        dp = [1] * m
+
+        for j in range(m):
+            for i in range(j):
+                if all(strs[r][i] <= strs[r][j] for r in range(n)):
+                    dp[j] = max(dp[j], dp[i] + 1)
+
+        return m - max(dp)
